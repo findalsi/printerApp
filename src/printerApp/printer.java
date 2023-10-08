b28157092d3d54da9b6ca42e3ea2353f1671d600
Dalsi Samarathunga <findalsidu@gmail.com>
2023-10-08 07:03:29 +0530

    Initial commit

---
+++ src/printerApp/printer.java
@@ -0,0 +1,55 @@
+package printerApp;
+
+public class printer {
+
+	public static void main(String[] args) {
+		System.out.println("Printer App");
+
+		int age = 38;
+		String name;
+		name = "Dalsi";
+		
+		System.out.println(age);
+		
+     	System.out.println("My name is");
+		System.out.println(name);
+	
+		System.out.println("My name is " + name);
+		System.out.println("I am " + age + " years old");
+		
+		int number = 5;
+		number = 2;
+		
+		number = 10 * 5;
+		
+		number = number + 1;
+		number++;
+		number--;
+		
+		double result;
+		
+		result = number / 3.3;
+		
+		System.out.println(result);
+		
+		System.out.printf("The result is %.2f \n\n", result);
+		
+		System.out.printf("My name is %s and I am %d years old.",name,age);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
